b85ec33d8fc51c77df3b7d744b47a8c1f9bef82f
dthcsm <cypcsm@gmail.com>
2023-01-06 03:04:32 +0900

    rèfactor code: create routes, app/controller

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 //HTTP logger
@@ -17,13 +19,20 @@ app.engine('hbs', engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Routes init
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// app.get('/', (req, res) => {
+//   res.render('home');
+// })
+
+// app.get('/news', (req, res) => {
+//   res.render('news');
+// })
+
+// app.post('/search', (req, res) => {
+//   res.send('');
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
